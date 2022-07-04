132bad9fe00bc8f4969c1bcbb801f76c6b90053e
dodain <dodain@pop-os.localdomain>
2022-07-04 16:40:54 +0500

    changed github org to Dodain

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule SErpi4.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
+  @github_organization "Dodain"
   @app :se_rpi4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
